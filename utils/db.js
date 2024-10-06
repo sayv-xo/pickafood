05a498937971a095e7e1d02008c8e00eadb3dca5
sayv-xo <98850746+sayv-xo@users.noreply.github.com>
2024-10-06 14:21:25 +0000

    fix mongo client connection and typo

---
+++ utils/db.js
@@ -0,0 +1,36 @@
+import { MongoClient } from 'mongodb';
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_NAME = process.env.DB_NAME || 'pickafood';
+const DB_URL = `mongodb://${DB_HOST}:${DB_PORT}`;
+
+class DBCLient {
+  constructor() {
+    this.client = new MongoClient(DB_URL, { useUnifiedTopology: true });
+    this.clientIsConnected = false;
+    
+    this.client.connect()
+      .then(() => {
+        this.clientIsConnected = true;
+      })
+      .catch(() => {
+        this.clientIsConnected = false;
+      });
+    this.db = this.client.db(DB_NAME);
+    this.usersCollection = this.db.collection('users');
+    this.menuCollection = this.db.collection('menu');
+    this.ordersCOllection - this.db.collection('orders');
+  }
+
+  isAlive() {
+    return this.clientIsConnected;
+  }
+
+  async nbUsers() {
+    return this.usersCollection.countDocuments();
+  }
+}
+
+const dbClient = new DBCLient();
+export default dbClient;
