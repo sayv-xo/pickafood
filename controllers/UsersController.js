e4584abf3a6b5d234b4898f8d79986e71cfaf48e
sayv <98850746+sayv-xo@users.noreply.github.com>
2024-10-17 22:55:56 +0000

    add order field to user schema

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,9 @@
 import sha256 from 'sha256';
 import dbClient from '../utils/db';
+import 'dotenv/config';
+import JwtService from '../utils/jwt';
+import redisClient from '../utils/redis';
+import { ObjectId } from 'mongodb';
 
 class UsersController {
   // creates a new user
@@ -25,15 +29,46 @@ class UsersController {
       return res.status(400).send({ error: 'Email already exist' });
     }
 
-    const newUser = await dbClient.usersCollection.insertOne({ username, email, password: sha256(password) });
-    return res.status(201).send({ message: `User ${username} created successfully.` });
+    const userId = new ObjectId();
+
+    const newUser = {
+      _id: userId,
+      username,
+      email,
+      orders: [],
+      password: sha256(password),
+    };
+    await dbClient.usersCollection.insertOne(newUser);
+    return res.status(201).send({ message: `User ${username} with id ${userId} created successfully.` });
+  }
+
+  static async getMe(req, res) {
+    const token = req.header('X-Token');
+
+    if (!token) {
+      return res.status(401).send({ error: 'Unauthorized' });
+    }
+
+    const privateKey = process.env.JWT_SECRET || 'pickafood';
+
+    try {
+      const decoded = JwtService.verify(token, privateKey);
+      const key = `auth_${token}`;
+      const userId = await redisClient.get(key);
+
+      if (!userId) {
+        return res.sendStatus(401);
+      }
+      
+      const users = dbClient.usersCollection;
+      const idObject = new ObjectId(userId);
+      const user = await users.findOne({ _id: idObject });
+      return res.status(200).send({ username: user.username, email: user.email, orders: user.orders });
+
+    } catch (err) {
+      return res.status(401).send({ error: ' Unauthorized' });
+    }
   }
 }
-
-
-
-
-
-
 
 export default UsersController;
